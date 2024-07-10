15c5b0502cdf1e7c69b1f69f8a4657b0f205496e
Nobbisaurio <davidxdcastro@gmail.com>
2024-07-10 13:29:48 -0500

    update url base in enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // urlBase: 'https://apicomplexivo.app.32bits.me/api',
-  urlBase: 'http://158.220.125.115:4900/api',
+  // urlBase: 'http://158.220.125.115:4900/api',
+  urlBase: 'https://api.gea.yavirac.org/',
 };
