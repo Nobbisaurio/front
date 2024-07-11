53264c8a4f4d7836240a8fac3bc45e4698871c47
root <root@vmi1732555.contaboserver.net>
2024-07-11 03:14:15 +0200

    change env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // urlBase: 'https://apicomplexivo.app.32bits.me/api',
   // urlBase: 'http://158.220.125.115:4900/api',
-  urlBase: 'https://api.gea.yavirac.org/',
+  urlBase: 'https://api.gae.yavirac.org/api',
 };
